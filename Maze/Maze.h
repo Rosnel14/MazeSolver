75e9a4f96fc3434f264531be7f8a474eec617713
Rosnel Alejandro Leyva-Cortes <30304057+Rosnel14@users.noreply.github.com>
2021-04-15 10:20:11 -0400

    maze obj mostly done

--- Maze/Maze.h
+++ Maze/Maze.h
@@ -12,19 +12,36 @@
 #import "Queue.h"
 
 
+//for size options 0 = small, 1=medium, 2= large, 3=huge
+
 @interface Maze : NSObject
 
+//this stack will be used for
+//pathfinding using DFS
+@property Stack * pathStack;
 
+//this queue will be used for
+//pathfinding using BFS
+@property Queue * pathQueue;
 
+//this will determine how big the maze is
+//base of what file is passed 
+@property NSString * fileNameFromSize;
 
-//constructor
+//constructor as blank maze
 -(instancetype)init;
+
+//construct as a DFS maze
+-(instancetype)initWithStackAndSize:(int)size;
+
+//construct as a BFS maze
+-(instancetype)initWithQueueAndSize:(int)size;
 
 //solve method, return true or false if the maze can solved
 -(bool)solve;
 
-//construct with a pre-determined filename 
--(instancetype)initWithSize:(NSString *)filename;
+//construct with a pre-determined filename
+-(instancetype)initWithSize:(int)size;
 
 @end
 
--- Maze/Maze.m
+++ Maze/Maze.m
@@ -7,4 +7,117 @@
 
 #import <Foundation/Foundation.h>
 #import "Maze.h"
+#import "ListNode.h"
+#import "Stack.h"
+#import "Queue.h"
 
+@implementation Maze
+
+-(instancetype)init {
+    if (self) {
+        self = [super init];
+        self.pathQueue = [[Queue alloc] init];
+        self.pathStack = [[Stack alloc]init];
+        self.pathStack = nil;
+        self.pathQueue = nil;
+        self.fileNameFromSize = nil;
+    }
+    return self;
+}
+
+-(instancetype)initWithStackAndSize:(int)size {
+    //this is just a placeholder until I figure out how to find file
+    NSString * filename;
+    switch (size) {
+        case 0:
+            filename = @"small";
+            break;
+        case 1:
+            filename = @"medium";
+            break;
+        case 2:
+            filename = @"large";
+            break;
+        case 3:
+            filename = @"huge";
+            break;
+        default:
+            break;
+    }
+    
+    if(self) {
+        self = [super init];
+        self.pathStack = [[Stack alloc]init];
+        self.pathQueue =[[Queue alloc]init];
+        self.pathQueue = nil;
+        self.fileNameFromSize = filename;
+    }
+    return self;
+    
+}
+
+-(instancetype)initWithQueueAndSize:(int)size {
+    //this is just a placeholder string until I figure out how to find file
+    NSString * filename;
+    switch (size) {
+        case 0:
+            filename = @"small";
+            break;
+        case 1:
+            filename = @"medium";
+            break;
+        case 2:
+            filename = @"large";
+            break;
+        case 3:
+            filename = @"huge";
+            break;
+        default:
+            break;
+    }
+    
+    if(self) {
+        self = [super init];
+        self.pathStack = [[Stack alloc]init];
+        self.pathQueue =[[Queue alloc]init];
+        self.pathStack = nil;
+        self.fileNameFromSize = filename;
+    }
+    return self;
+}
+
+-(instancetype)initWithSize:(int)size {
+    //this is just a placeholder until I figure out how to find file
+    NSString * filename;
+    switch (size) {
+        case 0:
+            filename = @"small";
+            break;
+        case 1:
+            filename = @"medium";
+            break;
+        case 2:
+            filename = @"large";
+            break;
+        case 3:
+            filename = @"huge";
+            break;
+        default:
+            break;
+    }
+    
+    if(self) {
+        self = [super init];
+        self.pathStack = [[Stack alloc]init];
+        self.pathQueue =[[Queue alloc]init];
+        self.pathStack = nil;
+        self.pathQueue = nil;
+        self.fileNameFromSize = filename;
+    }
+    return self;
+}
+
+-(bool)solve {
+    return NO; //blegh
+}
+@end
