68ac88a1ac3e06410860381a9173cf6eaea1d6b9
Rosnel Alejandro Leyva-Cortes <30304057+Rosnel14@users.noreply.github.com>
2021-04-15 10:00:34 -0400

    maze obj header done, i think

--- Maze/Maze.h
+++ Maze/Maze.h
@@ -7,15 +7,24 @@
 
 #ifndef Maze_h
 #define Maze_h
+#import "ListNode.h"
+#import "Stack.h"
+#import "Queue.h"
+
 
 @interface Maze : NSObject
 
-@property int mazeSize;
 
 
+
+//constructor
+-(instancetype)init;
 
 //solve method, return true or false if the maze can solved
--(bool)solve
+-(bool)solve;
+
+//construct with a pre-determined filename 
+-(instancetype)initWithSize:(NSString *)filename;
 
 @end
 
---
+++ Maze/Maze.m
@@ -0,0 +1,10 @@
+//
+//  Maze.m
+//  Maze
+//
+//  Created by Rosnel Leyva-Cortés on 4/15/21.
+//
+
+#import <Foundation/Foundation.h>
+#import "Maze.h"
+
